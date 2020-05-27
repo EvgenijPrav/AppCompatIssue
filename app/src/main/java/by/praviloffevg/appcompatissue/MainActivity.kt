d3f2c68621623f318c67d1b9d3aefac96642719c
Yauheni Pravilau <praviloffevg@gmail.com>
2020-05-27 15:54:43 +0300

    Initial commit

---
+++ app/src/main/java/by/praviloffevg/appcompatissue/MainActivity.kt
@@ -0,0 +1,12 @@
+package by.praviloffevg.appcompatissue
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
